3335a773c11137bd16bb8920c34aca9fbf50fbab
Deep Muni <deep.muni94@gmail.com>
2020-04-27 20:28:56 -0300

    nav-bar created

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,18 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavBarComponent } from './nav-bar/nav-bar.component';
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    NavBarComponent
-  ],
+    declarations: [
+        AppComponent,
+        NavBarComponent,
+        NavBarComponent
+    ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/nav-bar/nav-bar.component.ts
+++ src/app/nav-bar/nav-bar.component.ts
@@ -7,6 +7,19 @@ import { Component, OnInit } from '@angular/core';
 })
 export class NavBarComponent implements OnInit {
 
+  public itemsLeft = [
+    {'name': 'Home', 'url': ''},
+    {'name': 'Show', 'url': ''},
+    {'name': 'About Us', 'url': 'aboutUs'},
+    {'name': 'Contact Us', 'url': 'contactUs'}
+  ];
+
+  public itemsRight = [
+    {'name': 'Reset', 'url': 'reset'},
+    {'name': 'Login', 'url': 'login'},
+    {'name': 'Register', 'url': 'register'}
+  ];
+
   constructor() { }
 
   ngOnInit() {
